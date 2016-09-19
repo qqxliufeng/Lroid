6572e2d9061dc81ad2e27cd2ec9d11374256cb94
feng <596484713@qq.com>
2016-09-19 15:21:41 +0800

    2016年9月19日15:21:37

--- src/main/java/com/android/lf/lroid/v/fragment/LoginNormalFragment.java
+++ src/main/java/com/android/lf/lroid/v/fragment/LoginNormalFragment.java
@@ -1,7 +1,11 @@
 package com.android.lf.lroid.v.fragment;
 
+import android.text.Editable;
+import android.text.TextUtils;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 import com.android.lf.lroid.R;
 import com.android.lf.lroid.component.DaggerInjectPresentComponent;
@@ -17,6 +21,10 @@ public class LoginNormalFragment extends LroidBaseFragment {
 
     @BindView(R.id.id_bt_login_normal_login)
     Button mLogin;
+    @BindView(R.id.id_et_fragment_login_normal_user_name)
+    EditText mUserName;
+    @BindView(R.id.id_et_fragment_login_normal_password)
+    EditText mPassword;
 
     public static LoginNormalFragment newInstance() {
         return  new LoginNormalFragment();
@@ -29,15 +37,46 @@ public class LoginNormalFragment extends LroidBaseFragment {
 
     @Override
     protected void initView(View view) {
-        mLogin.setOnClickListener(new View.OnClickListener() {
+        mUserName.addTextChangedListener(new TextWatcher() {
             @Override
-            public void onClick(View v) {
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                if (!TextUtils.isEmpty(mUserName.getText().toString()) && !TextUtils.isEmpty(mPassword.getText().toString())){
+                    mLogin.setEnabled(true);
+                }else {
+                    mLogin.setEnabled(false);
+                }
+            }
+        });
+        mPassword.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                if (!TextUtils.isEmpty(mUserName.getText().toString()) && !TextUtils.isEmpty(mPassword.getText().toString())){
+                    mLogin.setEnabled(true);
+                }else {
+                    mLogin.setEnabled(false);
+                }
             }
         });
     }
 
     @Override
     protected void setComponent() {
-        DaggerInjectPresentComponent.builder().presentModule(new PresentModule()).build().inject(this);
+//        DaggerInjectPresentComponent.builder().presentModule(new PresentModule()).build().inject(this);
     }
 }
