211c212a91d0c911682ef2d052a90a80b0a3acb3
feng <596484713@qq.com>
2016-10-24 18:15:30 +0800

    2016年10月24日18:15:25

--- src/main/java/com/android/lf/lroid/v/adapter/FoodContentAdapter.java
+++ src/main/java/com/android/lf/lroid/v/adapter/FoodContentAdapter.java
@@ -1,6 +1,11 @@
 package com.android.lf.lroid.v.adapter;
 
+import android.text.Html;
+
+import com.android.lf.lroid.R;
 import com.android.lf.lroid.m.bean.FoodInfoBean;
+import com.android.lf.lroid.volley.RequestManager;
+import com.android.volley.toolbox.NetworkImageView;
 import com.chad.library.adapter.base.BaseViewHolder;
 
 import java.util.List;
@@ -16,6 +21,17 @@ public class FoodContentAdapter extends LroidBaseRecycleViewAdapter<FoodInfoBean
 
     @Override
     protected void convert(BaseViewHolder baseViewHolder, FoodInfoBean o) {
-
+        baseViewHolder.setText(R.id.id_tv_adapter_food_content_item_title,o.getName());
+        baseViewHolder.setText(R.id.id_tv_adapter_food_content_item_summary,o.getRecipe().getSumary());
+        String step = "共分为 <p style=color:red>"+ o.getRecipe().getMethod().size() +"</p> 步";
+        baseViewHolder.setText(R.id.id_tv_adapter_food_content_item_step,Html.fromHtml(step));
+        NetworkImageView pic = baseViewHolder.getView(R.id.id_iv_adapter_food_content_item_pic);
+        pic.setDefaultImageResId(R.drawable.drawable_image_request_default);
+        pic.setErrorImageResId(R.drawable.drawable_image_request_default);
+        if (o.getThumbnail()!=null){
+            pic.setImageUrl(o.getThumbnail(), RequestManager.getImageLoader());
+        }else {
+            pic.setImageUrl("", RequestManager.getImageLoader());
+        }
     }
 }
