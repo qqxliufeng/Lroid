a046c824e720adf98f2f7a8bfa558ccc462a74fe
feng <596484713@qq.com>
2016-10-31 16:20:29 +0800

    2016年10月31日17:57:48

--- src/main/java/com/android/lf/lroid/v/views/CustomDialogHelper.java
+++ src/main/java/com/android/lf/lroid/v/views/CustomDialogHelper.java
@@ -2,7 +2,10 @@ package com.android.lf.lroid.v.views;
 
 import android.app.Dialog;
 import android.content.Context;
+import android.view.Gravity;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 
 import com.android.lf.lroid.R;
 
@@ -16,6 +19,11 @@ public class CustomDialogHelper {
         Dialog emptyDialog = new Dialog(context, R.style.emptyDialog);
         View emptyView = View.inflate(context,R.layout.dialog_empty_layout,null);
         emptyDialog.setContentView(emptyView);
+        Window window = emptyDialog.getWindow();
+        WindowManager.LayoutParams lp = window.getAttributes();
+        window .setGravity(Gravity.CENTER | Gravity.TOP);
+        lp.y = 200;
+        window.setAttributes(lp);
         emptyDialog.show();
     }
 }
